262cb20c8f6f4c9cd87b52e4262f9b2ba5f6c543
ligang-chn <ligang.she@gmail.com>
2020-02-17 00:20:09 +0800

    softmax#2020/2/17

--- d2lzh_pytorch/utils.py
+++ d2lzh_pytorch/utils.py
@@ -1,5 +1,8 @@
 import random
+import sys
 
+import torchvision
+import torchvision.transforms as transforms
 import torch
 from IPython import display
 import matplotlib.pyplot as plt
@@ -39,3 +42,97 @@ def data_iter(batch_size,features,labels):
         j=torch.LongTensor(indices[i:min(i+batch_size,num_examples)])# 最后一次可能不足一个batch
         yield features.index_select(0,j),labels.index_select(0,j)
         #注意这里使用的是yield，因为当一个循环结束时发送出一个批量，然后返回的到刚刚离开的位置
+
+
+def get_fashion_mnist_labels(labels):
+    """
+    将数值标签转换成相应的文本标签
+    :param labels:数值标签
+    :return:文本标签列表
+    """
+    text_labels=['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+def show_fashion_mnist(images,labels):
+    """
+    绘制多张图像和对应标签
+    :param images: 图像
+    :param labels: 标签
+    :return: 图片
+    """
+    use_svg_display()
+    #这里的_表示我们忽略（不使用）的变量
+    _,figs=plt.subplots(1,len(images),figsize=(12,12))
+    for f,img,lbl in zip(figs,images,labels):
+        f.imshow(img.view(28,28).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+
+def load_data_fashion_mnist(batch_size, root='./Datasets/FashionMNIST0216'):
+    """Download the fashion mnist dataset and then load into memory."""
+    transform = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+
+def evaluate_accuracy(data_iter,net):
+    acc_sum,n=0.0,0
+    for X,y in data_iter:
+        acc_sum+=(net(X).argmax(dim=1)==y).float().sum().item()
+        n+=y.shape[0]
+    return acc_sum/n
+
+
+def sgd(params,lr,batch_size):
+    """
+    小批量随机梯度下降算法
+    :param params:需要迭代的参数
+    :param lr: 学习率
+    :param batch_size:批量大小
+    :return: 迭代得到的参数
+    """
+    for param in params:
+        param.data -= lr*param.grad/batch_size#注意更改param时使用param。data
+
+
+def train_ch3(net,train_iter,test_iter,loss,num_epochs,batch_size,
+              params=None,lr=None,optimizer=None):
+    for epoch in range(num_epochs):
+        train_l_sum,train_acc_sum,n=0.0,0.0,0
+        for X,y in train_iter:
+            y_hat=net(X)
+            l=loss(y_hat,y).sum()
+
+            #梯度清零
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+
+            l.backward()
+            if optimizer is None:
+                sgd(params,lr,batch_size)
+            else:
+                optimizer.step()#torch版本中用到
+
+            train_l_sum+=l.item()
+            train_acc_sum+=(y_hat.argmax(dim=1)==y).sum().item()
+            n+=y.shape[0]
+        test_acc=evaluate_accuracy(test_iter,net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
--- softmax.py
+++ softmax.py
@@ -1,3 +1,87 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import time
 import numpy as np
+import sys
+sys.path.append("..")
+import d2lzh_pytorch as d2l
+
+
+# mnist_train=torchvision.datasets.FashionMNIST(
+#     root='./Datasets/FashionMNIST0216',
+#     train=True,
+#     download=True,
+#     transform=transforms.ToTensor()
+# )
+# mnist_test=torchvision.datasets.FashionMNIST(
+#     root='./Datasets/FashionMNIST0216',
+#     train=False,
+#     download=True,
+#     transform=transforms.ToTensor()
+# )
+
+
+
+
+#设置批量大小
+batch_size=256
+train_iter,test_iter=d2l.load_data_fashion_mnist(batch_size)
+
+
+#初始化模型参数
+num_inputs=784
+num_outputs=10
+
+W=torch.tensor(np.random.normal(0,0.01,
+                                (num_inputs,num_outputs)),
+               dtype=torch.float)
+b=torch.zeros(num_outputs,dtype=torch.float)
+W.requires_grad_(requires_grad=True)
+b.requires_grad_(requires_grad=True)
+
+
+#实现softmax运算
+def softmax(X):
+    X_exp=X.exp()#首先对每个元素做指数运算
+    partition=X_exp.sum(dim=1,keepdim=True)#对同一行数据进行求和
+    return X_exp/partition #这里使用了广播机制
+
+
+#定义模型
+def net(X):
+    return softmax(torch.mm(X.view((-1,num_inputs)),W)+b)
+
+
+#定义损失函数
+def cross_entropy(y_hat,y):
+    return -torch.log(y_hat.gather(1,y.view(-1,1)))
+
+
+#计算分类准确率
+def accuracy(y_hat,y):
+    return (y_hat.argmax(dim=1)==y).float().mean().item()
+
+
+#训练模型
+num_epochs,lr=5,0.1
+d2l.train_ch3(net, train_iter, test_iter,
+          cross_entropy, num_epochs,
+          batch_size,
+          [W, b], lr)
+
+if __name__=='__main__':
+
+    # print(type(mnist_train))
+    # print(len(mnist_train), len(mnist_test))
+    # print(d2l.evaluate_accuracy(test_iter,net))
+
+    X,y=iter(test_iter).next()
+
+    true_labels=d2l.get_fashion_mnist_labels(y.numpy())
+    pred_labels=d2l.get_fashion_mnist_labels(net(X).argmax(dim=1).numpy())
+    titles = [true + '\n' + pred for true, pred in zip(true_labels, pred_labels)]
+
+    d2l.show_fashion_mnist(X[0:9], titles[0:9])
+
