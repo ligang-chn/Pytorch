0220380d3e6f5defbe4d82d0b63ab3dd8b60a945
ligang-chn <ligang.she@gmail.com>
2020-02-18 18:34:04 +0800

    RNN#2020/2/18

--- d2lzh_pytorch/utils.py
+++ d2lzh_pytorch/utils.py
@@ -1,6 +1,8 @@
 import random
 import sys
+import time
 
+import math
 import torchvision
 import torchvision.transforms as transforms
 import torch
@@ -165,11 +167,11 @@ def load_data_jay_lyrics():
 
 
 def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    """ 随机采样 """
     # 减1是因为输出的索引x是相应输入的索引y加1
-    num_examples = (len(corpus_indices) - 1) // num_steps
-    epoch_size = num_examples // batch_size
-    example_indices = list(range(num_examples))
-    random.shuffle(example_indices)
+    num_examples = (len(corpus_indices) - 1) // num_steps#向下取整，得到不重叠的样本个数
+    example_indices = [i*num_steps for i in list(range(num_examples))] #每个样本的第一个字符在corpus_indices中的下标
+    random.shuffle(example_indices) #打乱顺序
 
     # 返回从pos开始的长为num_steps的序列
     def _data(pos):
@@ -177,24 +179,24 @@ def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    for i in range(epoch_size):
+    for i in range(0, num_examples, batch_size):
         # 每次读取batch_size个随机样本
-        i = i * batch_size
-        batch_indices = example_indices[i: i + batch_size]
-        X = [_data(j * num_steps) for j in batch_indices]
-        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        batch_indices = example_indices[i: i + batch_size]#当前batch的各个样本的首字符的下标
+        X = [_data(j) for j in batch_indices]
+        Y = [_data(j + 1) for j in batch_indices]
         yield torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(Y, dtype=torch.float32, device=device)
 
 
 def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    """ 相邻采样 """
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    corpus_indices = torch.tensor(corpus_indices, dtype=torch.float32, device=device)
-    data_len = len(corpus_indices)
-    batch_len = data_len // batch_size
-    indices = corpus_indices[0: batch_size*batch_len].view(batch_size, batch_len)
-    epoch_size = (batch_len - 1) // num_steps
-    for i in range(epoch_size):
+    corpus_len = len(corpus_indices) // batch_size * batch_size  # 保留下来的序列的长度
+    corpus_indices = corpus_indices[: corpus_len]  # 仅保留前corpus_len个字符
+    indices = torch.tensor(corpus_indices, device=device)
+    indices = indices.view(batch_size, -1)  # resize成(batch_size, )
+    batch_num = (indices.shape[1] - 1) // num_steps
+    for i in range(batch_num):
         i = i * num_steps
         X = indices[:, i: i + num_steps]
         Y = indices[:, i + 1: i + num_steps + 1]
@@ -203,18 +205,119 @@ def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
 
 
 
+def grad_clipping(params,theta,device):
+    """裁剪梯度
+    循环神经网络容易出现梯度衰减或梯度爆炸
+    把所有模型参数梯度的元素拼接成一个向量g，并设置裁剪的阈值theta
+    """
+    norm=torch.tensor([0.0],device=device)
+    for param in params:
+        norm+=(param.grad.data**2).sum()
+    norm=norm.sqrt().item()  # L2范数
+    if norm>theta:
+        for param in params:
+            param.grad.data*=(theta/norm)#分母就是L2范数
 
 
+#one-hot变量
+def one_hot(x,n_class,dtype=torch.float32):
+    # X shape: (batch), output shape: (batch, n_class)
+    x=x.long()
+    res=torch.zeros(x.shape[0],n_class,dtype=dtype,device=x.device)
+    res.scatter_(1,x.view(-1,1),1)
+    # scatter_(input, dim, index, src)
+    # 将src中数据根据index中的索引按照dim的方向填进input中.
+    return res
 
 
+def to_onehot(X,n_class):
+    #X shape: (batch, seq_len), output: seq_len elements of (batch, n_class)
+    return [one_hot(X[:,i],n_class) for i in range(X.shape[1])]
 
 
+class RNNModel(nn.Module):
+    def __init__(self, rnn_layer, vocab_size):
+        super(RNNModel, self).__init__()
+        self.rnn = rnn_layer
+        self.hidden_size = rnn_layer.hidden_size * (2 if rnn_layer.bidirectional else 1)
+        self.vocab_size = vocab_size
+        self.dense = nn.Linear(self.hidden_size, vocab_size)
+        self.state = None
+
+    def forward(self, inputs, state): # inputs: (batch, seq_len)
+        # 获取one-hot向量表示
+        X = to_onehot(inputs, self.vocab_size) # X是个list
+        Y, self.state = self.rnn(torch.stack(X), state)
+        # 全连接层会首先将Y的形状变成(num_steps * batch_size, num_hiddens)，它的输出
+        # 形状为(num_steps * batch_size, vocab_size)
+        output = self.dense(Y.view(-1, Y.shape[-1]))
+        return output, self.state
 
 
+def predict_rnn_pytorch(prefix, num_chars, model, vocab_size, device, idx_to_char,
+                      char_to_idx):
+    state = None
+    output = [char_to_idx[prefix[0]]] # output会记录prefix加上输出
+    for t in range(num_chars + len(prefix) - 1):
+        X = torch.tensor([output[-1]], device=device).view(1, 1)
+        if state is not None:
+            if isinstance(state, tuple): # LSTM, state:(h, c)
+                state = (state[0].to(device), state[1].to(device))
+            else:
+                state = state.to(device)
+
+        (Y, state) = model(X, state)
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t + 1]])
+        else:
+            output.append(int(Y.argmax(dim=1).item()))
+    return ''.join([idx_to_char[i] for i in output])
 
 
+def train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
+                                corpus_indices, idx_to_char, char_to_idx,
+                                num_epochs, num_steps, lr, clipping_theta,
+                                batch_size, pred_period, pred_len, prefixes):
+    loss = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    model.to(device)
+    state = None
+    for epoch in range(num_epochs):
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_consecutive(corpus_indices, batch_size, num_steps, device) # 相邻采样
+        for X, Y in data_iter:
+            if state is not None:
+                # 使用detach函数从计算图分离隐藏状态, 这是为了
+                # 使模型参数的梯度计算只依赖一次迭代读取的小批量序列(防止梯度计算开销太大)
+                if isinstance (state, tuple): # LSTM, state:(h, c)
+                    state = (state[0].detach(), state[1].detach())
+                else:
+                    state = state.detach()
 
+            (output, state) = model(X, state) # output: 形状为(num_steps * batch_size, vocab_size)
 
+            # Y的形状是(batch_size, num_steps)，转置后再变成长度为
+            # batch * num_steps 的向量，这样跟输出的行一一对应
+            y = torch.transpose(Y, 0, 1).contiguous().view(-1)
+            l = loss(output, y.long())
 
+            optimizer.zero_grad()
+            l.backward()
+            # 梯度裁剪
+            grad_clipping(model.parameters(), clipping_theta, device)
+            optimizer.step()
+            l_sum += l.item() * y.shape[0]
+            n += y.shape[0]
 
+        try:
+            perplexity = math.exp(l_sum / n)
+        except OverflowError:
+            perplexity = float('inf')
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, perplexity, time.time() - start))
+            for prefix in prefixes:
+                print(' -', predict_rnn_pytorch(
+                    prefix, pred_len, model, vocab_size, device, idx_to_char,
+                    char_to_idx))
 
